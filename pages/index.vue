f1b0c301b0413c3cd6880184923b4a57c6e6d281
Masanori Ikeda <sintokui3141592@gmail.com>
2025-11-24 20:23:18 +0900

    second commit

---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div>
+      <h1>Counter</h1>
+      <button v-on:click="increment">
+        {{count}}
+      </button>
+    </div>
+    <div>
+      <h1>{{title}}</h1>
+      <input type="text" v-model="inputText">
+      <button v-on:click="onClick">Add</button>
+      <TodoItem
+        v-for="(task, key) in tasks"
+        :task="task"
+        :key="key"
+        @delete-task="deleteTask"></TodoItem>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from '../components/ToDoItem.vue'
+export default {
+  components: {
+        TodoItem
+  },
+  data: () => ({
+    count: 0,
+    title: "To Do Appli",
+    inputText: "",
+    tasks: [],
+    is_checked: false,
+  }),
+  methods: {
+    increment: function(){
+      this.count++
+    },
+    onClick: function(){
+      this.message = this.inputText
+      this.tasks.push(this.inputText)
+    },
+    deleteTask: function(taskToDelete){
+      const index = this.tasks.indexOf(taskToDelete)
+      if(index !== -1){
+        this.tasks.splice(index, 1)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
